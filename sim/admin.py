cb86a68ba667340bfa1c6ef3a6aa4ccb48a443d7
Vlad Seliverstov <behebot@gmail.com>
2024-11-28 20:10:10 -0700

    add admin page support

--- sim/admin.py
+++ sim/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Author, Message
 
-# Register your models here.
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ['name']
+    search_fields = ['name']
+
+@admin.register(Message)
+class MessageAdmin(admin.ModelAdmin):
+    list_display = ('author', 'created_at', 'content')
+    search_fields = ('author', 'content')
