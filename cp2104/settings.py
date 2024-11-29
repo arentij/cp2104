e15d06f0544799971a9e153b13a425d46c83ddb9
Vlad Seliverstov <behebot@gmail.com>
2024-11-28 19:53:38 -0700

    add messenger

--- cp2104/settings.py
+++ cp2104/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'daphne',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -38,7 +39,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'shards',
+    'sim'
 ]
+
+ASGI_APPLICATION = 'cp2104.asgi.application'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
