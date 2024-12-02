dafcbebd3b82c72d3f633925bd9f876e7965ccea
Vlad Seliverstov <behebot@gmail.com>
2024-12-01 21:53:40 -0700

    add login/logout/toc

--- character/urls.py
+++ character/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('login/', views.custom_login, name='login'),
+    path('logout/', views.custom_logout, name='logout'),
     path('character/<int:character_id>/', views.character_page, name='character_page'),
+    path('toc/', views.toc, name='toc'),
 ]
