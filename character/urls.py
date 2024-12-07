e6859dc2f5aef34b80a6b7f4aa2cbdbb9a9f1ab8
Vlad Seliverstov <behebot@gmail.com>
2024-12-06 18:08:56 -0700

    rename /character handler to /data

--- character/urls.py
+++ character/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     path('login/', views.custom_login, name='login'),
     path('logout/', views.custom_logout, name='logout'),
-    path('character/<int:character_id>/', views.character_page, name='character_page'),
+    path('data/<int:character_id>/', views.character_page, name='character_page'),
     path('toc/', views.toc, name='toc'),
 ]
